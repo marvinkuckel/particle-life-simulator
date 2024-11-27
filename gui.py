16a3d532fd18f0c2517332107e1a5133a3cccff8
BelizSnl <belizsenol@gmail.com>
2024-11-27 16:58:25 +0100

    basic structure created

---
+++ gui.py
@@ -0,0 +1,26 @@
+import pygame 
+
+class Button(): 
+    def __init__():
+        pass
+    
+    def start_button():
+        pass
+    
+    def pause_button(): 
+        pass
+    
+    def trigger():
+        #durch klicken wird getriggert
+        pass
+
+class GUI(): 
+    def __init__():
+        pass   
+     
+    def board():
+        pass
+    
+    def control_panel():
+        #this is where the bar should go where you can adjust color and interaction
+        pass
