63a4fda934d481182e9c76c687acb7e647a1526c
BelizSnl <belizsenol@gmail.com>
2024-12-04 11:57:45 +0100

    init, draw board and draw control panel in GUI class done

--- gui.py
+++ gui.py
@@ -3,15 +3,12 @@ import pygame
 class Button(): 
     def __init__(self, x, y, width, height, text, color):
         """
-        Initialize a button.
         x: X-coordinate of the button
         y: Y-coordinate of the button
         width: Button width
         height: Button height
         text: Text displayed on the button
         color: Default color of the button
-        hover_color: Color of the button when hovered
-        action: Function to trigger on button click
         """
         self.rect = pygame.Rect(x, y, width, height)
         self.text = text
@@ -52,64 +49,22 @@ class Button():
 
 
 class GUI(): 
-    def __init__():
-        pass   
+    def __init__(self, screen, screen_width, screen_height, control_panel_width):
+        self.screen = screen
+        self.screen_width = screen_width
+        self.screen_height = screen_height
+        self.control_panel_width = control_panel_width
+        self.buttons = []
+        self.setup_controls() 
      
-    def board():
-        pass
+    def board(self):
+        #Draw the main game board
+        pygame.draw.rect(self.screen, (30, 30, 30), (0, 0, self.screen_width, self.screen_height))
     
-    def control_panel():
+    
+    def control_panel(self):
         #this is where the bar should go where you can adjust color and interaction
-        pass
+        pygame.draw.rect(self.screen, (0, 0, 0), (self.screen_width, 0, self.control_panel_width, self.screen_height))  # Black control panel
 
 
-'''
-
-
-class ParticleLifeGUI():
-    def __init__(self, canvas_size: Tuple[int, int], n_particles: int, fps=60):
-        pygame.init()
-        
-        self.window = pygame.display.set_mode(canvas_size)
-        # screen-updates/frames per second
-        self.fps = fps
-        self.simulation = PLSimulation(n_particles, canvas_size)
     
-    def run(self):
-        clock = pygame.time.Clock()
-        dt = 0  # delta-time is used to make particle movements dependent on time (time since last screen update), to avoid different speeds for different fps
-        
-        self.running = True
-        while self.running:
-            self.event_loop()
-            # time passed since last clock.tick()
-            dt = clock.tick(self.fps) - dt
-            self.draw_canvas(dt)
-    
-    def event_loop(self):
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                self.running = False
-                pygame.quit()
-                sys.exit()
-            
-            # if event.type == pygame.VIDEORESIZE:
-            #     self.window = pygame.display.set_mode((event.w, event.h), pygame.RESIZABLE)
-        
-    def draw_canvas(self, dt):
-        # background
-        self.window.fill(10)
-        
-        # update particle positions
-        positions = self.simulation.step(dt)
-        
-        # draw updated particles on canvas
-        for particle in positions:
-            pygame.draw.circle(self.window, (255, 255,  255), particle, 1)
-        # update screen with updated canvas
-        pygame.display.update()
-
-if __name__ == "__main__":
-    app = ParticleLifeGUI(canvas_size=(800,800), n_particles=1000)
-    app.run()
-    '''
