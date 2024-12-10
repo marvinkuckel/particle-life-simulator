328b91cf334adf0238a770c0d4ff9721231d54db
Marc-Maxelon <marc.maxelon@study.hs-duesseldorf.de>
2024-12-10 14:13:54 +0100

    New methods in simulation based on uploaded code

--- Simulation.py
+++ Simulation.py
@@ -5,25 +5,86 @@ interact with the particle.py and interactions.py files to manage particle prope
 """
 
 import pygame
+import random
 from particle import Particle
 from interactions import InteractionMatrix
 
 class Simulation:
-    def __init__(self, width, height, num_particles = 2000):
+    def __init__(self, width, height, num_particles = 2000, number_of_types = 3, interaction_radius = 10):
         """
         Initializes the simulation with the specified dimensions and number of particles.
 
         width: The width of the simulation area.
         height: The height of the simulation area.
         num_particles: The number of particles to generate.
+        num_types: The number of particle types.
+        interaction_radius: The interaction radius between particles.
         """
         self.width = width
         self.height = height
         self.num_particles = num_particles
+        self.num_types = number_of_types
+        self.interaction_radius = interaction_radius
+        self.particles = []
+        self.interaction_matrix = InteractionMatrix(self.number_of_types, self.interaction_radius)
+        self.particle_colors = {
+            'christmas-red': (220, 20, 60),
+            'christmas-green': (0, 128, 0),
+            'christmas-white': (255, 255, 255),
+            'christmas-gold': (255, 215, 0)
+        }
 
     def setup_simulation(self):
         """
-        Sets up the initial state of the simulation, including creating particles.
+        Applies the methods to create random particles and initialize the interaction matrix.
+        """
+        pass
+    
+    def create_particles(self):
+        """
+        Creates random particles and adds them to the simulation.
+        """
+        pass
+    
+    def particle_position(self):
+        """
+        Returns a random position within the simulation area.
+        """
+        pass
+
+    def define_particle_colors(self, particle_type):
+        """
+        Uses the dictionary of particle colors to assign a color to a particle type.
+        """
+        pass
+
+    def particle_size(self):
+        """
+        Returns a random size for a particle.
+        """
+        pass
+    
+    def particle_velocity(self):
+        """
+        Returns a random velocity for a particle.
+        """
+        pass
+    
+    def particle_friction(self):
+        """
+        Returns a random friction value for a particle to make its movement appear more natural and realistic.
+        """
+        pass
+    
+    def particle_random_movement(self):
+        """
+        Returns a random random movement value for a particle.
+        """
+        pass
+
+    def initialize_interaction_matrix(self):
+        """
+        Initializes the interaction matrix for particle interactions.
         """
         pass
 
