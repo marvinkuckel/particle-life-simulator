87c739ec5b1a9bf1abee5676ed782b8ac2d91088
Marvin Kuckel <marvin.kuckel@outlook.de>
2024-12-11 09:47:52 +0100

    implemented first particle initialization, drawing on screen, update of particle position inclouding boundary enforcement

--- Simulation.py
+++ Simulation.py
@@ -3,6 +3,7 @@ The simulation.py file will contain the Simulation class responsible for creatin
 particles, handling their interactions, and ensuring the simulation performs efficiently. This class will
 interact with the particle.py and interactions.py files to manage particle properties and rules.
 """
+import random
 
 import pygame
 from particle import Particle
@@ -19,29 +20,46 @@ class Simulation:
         """
         self.width = width
         self.height = height
-        self.num_particles = num_particles
+        
+        self.setup_simulation()
 
-    def setup_simulation(self):
+    def setup_simulation(self, num_types: int):
         """
         Sets up the initial state of the simulation, including creating particles.
         """
+        self.particles = [Particle(type = i % num_types, color=None, size=1, 
+                                   position=(random.random(), random.random),
+                                   velocity=(1 - random.random*2, 1 - random.random*2), 
+                                   friction=0, random_movement=0) for i in range(self.num_particles)]
         pass
 
-    def render_frame(self):
+    def render_frame(self, screen: pygame.display):
         """
         Draws the current state of the simulation onto the screen.
         """
-        pass
+        for p in self.particles:
+            p.draw(screen)
 
-    def update_simulation(self):
+    def update(self, dt):
         """
         Updates the state of the simulation by calculating particle interactions,
         applying rules, and updating particle positions.
         """
-        pass
+        for p in self.particles:
+            p.update()
+            
+        self.enforce_boundaries()
 
     def enforce_boundaries(self):
         """
         Ensures that particles stay within the defined simulation boundaries.
         """
-        pass
+        for p in self.particles:
+            if p.position[0] < 0 or p.position[0] > 1:
+                p.velocity[0] = -p.velocity[0]
+                
+            if p.position[1] < 0 or p.position[1] > 1:
+                p.velocity[1] = -p.velocity[1]
+            
+            
+        
