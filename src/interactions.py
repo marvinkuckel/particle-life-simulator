e30741e007238ba35533128d74aa30573aee19fb
Nevo1109 <154609533+Nevo1109@users.noreply.github.com>
2025-01-23 17:14:01 +0100

    Update interactions.py
    doppelte __init__ entfernt

--- src/interactions.py
+++ src/interactions.py
@@ -1,27 +1,36 @@
 import random
-
 from particle import Particle
+
+# src/particle.py
+
+class Particle:
+    def __init__(self, type_id: int, size: int, position: tuple):
+        self.type = type_id  # Partikeltyp
+        self.size = size  # Partikelgröße
+        self.position = position  # Position des Partikels als Tupel (x, y)
+
 
 class InteractionMatrix:
     def __init__(self, num_types: int, default_radius: float):
         self.default_radius = default_radius
+        # Erstellen der Interaktionsmatrix
         self.interactions = {
             (i, j): [random.choice((1, -1)) * 0.5, self.default_radius, 1.0]  # Default strength is 1.0 (no attraction)
             for i in range(num_types) for j in range(num_types)
         }
 
     def add_attraction(self, type1, type2, strength):
-        # Set an attraction force between two types of particles
-        if type1 != type2:  # Typically, attraction is between different types
+        # Setzt eine Anziehungswirkung zwischen zwei Partikeltypen
+        if type1 != type2:  # Normalerweise ist Anziehung zwischen verschiedenen Typen
             self.interactions[(type1, type2)][2] = strength
-            self.interactions[(type2, type1)][2] = strength  # Symmetric interaction
+            self.interactions[(type2, type1)][2] = strength  # Symmetrische Interaktion
 
-    def calculate_force(self, p1, p2):
+    def calculate_force(self, p1: Particle, p2: Particle):
         force, radius, attraction = self.interactions[p1.type, p2.type]
         distance = self._distance(p1.position, p2.position)
 
         if distance <= radius:
-            applied_force = (force / distance ** 2) - attraction  # Apply attraction force
+            applied_force = (force / distance ** 2) - attraction  # Anziehende Kraft anwenden
             x_force = (p2.position[0] - p1.position[0]) * applied_force
             y_force = (p2.position[1] - p1.position[1]) * applied_force
             return x_force, y_force
@@ -31,43 +40,10 @@ class InteractionMatrix:
     def _distance(p1_pos, p2_pos):
         return ((p2_pos[0] - p1_pos[0]) ** 2 + (p2_pos[1] - p1_pos[1]) ** 2) ** 0.5
 
-    interactions: dict[(int, int), float]
 
-    def __init__(self, number_of_types: int, default_interaction_radius: float):
-        self.default_interaction_radius = default_interaction_radius
-        
-        self.interactions = {
-            (i, j): [random.choice((1, -1))*0.5, self.default_interaction_radius]
-            for i in range(number_of_types)
-            for j in range(number_of_types)
-        }
-        
-    def calculate_force(self, p1: Particle, p2: Particle):
-        force, radius = self.interactions[p1.type, p2.type]
-        
-        distance = self._distance(p1.position, p2.position)
-        if distance <= radius:
-            if distance > 0.005:
-                applied_force = (force / distance**2)-force# if distance < 1 else (force * distance**3)
-                x_force = (p2.position[0] - p1.position[0]) * applied_force
-                y_force = (p2.position[1] - p1.position[1]) * applied_force
-            else:
-                applied_force = 1 / ((distance+0.000001)**(distance))
-                x_force = -(p2.position[0] - p1.position[0]) * applied_force
-                y_force = -(p2.position[1] - p1.position[1]) * applied_force
-            return x_force, y_force
-        return 0, 0
-
-    def _distance(self, p1_pos, p2_pos):
-        return ((p2_pos[0] - p1_pos[0])**2 + (p2_pos[1] - p1_pos[1])**2)**0.5
-
-   # Beispiel: Anziehungskraft zwischen Partikeln vom Typ 0 und 1 mit einer Stärke von 0.1
 # tests/test_interaction_matrix.py
 import pytest
-
-
-from .interaction_matrix import InteractionMatrix
-
+from src.interaction_matrix import InteractionMatrix
 from src.particle import Particle
 
 def test_calculate_force_no_interaction():
