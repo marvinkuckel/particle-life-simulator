25c3f6411aa0e38dd1aadcc7ebb667881df6cc87
BelizSnl <belizsenol@gmail.com>
2025-02-11 00:31:48 +0100

    solve merge problem

--- src/interactions.py
+++ src/interactions.py
@@ -1,29 +1,16 @@
 import random
-from particle import Particle
-#from interaction_matrix import InteractionMatrix - erstmal nicht mehr drauf zugreifen
-
-'''
-# src/particle.py
-
-class Particle:
-    def __init__(self, type_id: int, size: int, position: tuple):
-        self.type = type_id  # Partikeltyp
-        self.size = size  # Partikelgröße
-        self.position = position  # Position des Partikels als Tupel (x, y)
-'''
-
-
-import random
 from math import sqrt, exp
 
 class InteractionMatrix:
     def __init__(self, num_types: int, default_radius: float):
+        self.number_of_types = num_types
         self.default_radius = default_radius
+        choice = lambda: random.choice((1, -1)) * random.choice((0, 0.2, 0.4, 0.6, 0.8, 1))
         self.min_distance = 0.05  #minimum allowed distance between particles (prevents touching & sticking)
         self.smooth_factor = 0.05  #strength of the smooth force reduction
         # creating Interaktionsmatrix
         self.interactions = {
-            (i, j): [random.choice((1, -1)) * 0.5, self.default_radius, 1.0]  # Default strength is 1.0 (no attraction)
+            (i, j): [choice(), self.default_radius, 1.0]  # Default strength is 1.0 (no attraction)
             for i in range(num_types) for j in range(num_types)
         }
 
@@ -61,29 +48,3 @@ class InteractionMatrix:
     @staticmethod
     def _distance(pos1, pos2):
         return sqrt((pos2[0] - pos1[0]) ** 2 + (pos2[1] - pos1[1]) ** 2) #eucludien distance between the 2 points
-
-
-# tests/test_interaction_matrix.py
-import pytest
-
-
-def test_calculate_force_no_interaction():
-    interaction_matrix = InteractionMatrix(num_types=4, default_radius=0.05)
-    p1 = Particle(type_id=0, size=2, position=(0.1, 0.1))
-    p2 = Particle(type_id=1, size=2, position=(0.1, 0.2))
-    
-    # Keine Wechselwirkung, wenn die Distanzen zu groß sind
-    force_x, force_y = interaction_matrix.calculate_force(p1, p2)
-    assert force_x == 0
-    assert force_y == 0
-
-def test_calculate_force_with_interaction():
-    interaction_matrix = InteractionMatrix(num_types=4, default_radius=0.1)
-    p1 = Particle(type_id=0, size=2, position=(0.1, 0.1))
-    p2 = Particle(type_id=1, size=2, position=(0.1, 0.15))
-    
-    # Berechnung der Wechselwirkung, wenn die Distanzen klein genug sind
-    force_x, force_y = interaction_matrix.calculate_force(p1, p2)
-    assert force_x != 0
-    assert force_y != 0
-
