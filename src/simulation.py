598a295f0b26b0e42403a131ea55a331f6be9431
Lilith Schmidl <lilith.schmidl@study.hs-duesseldorf.de>
2025-02-13 09:02:15 +0100

    improved comments and readability for simulation.py methods

--- src/simulation.py
+++ src/simulation.py
@@ -2,6 +2,7 @@ import random
 from particle import Particle
 from interactions import InteractionMatrix
 
+
 class Simulation:
     def __init__(self, width, height, interaction_matrix: InteractionMatrix, num_particles=1000, num_types=4, time_factor=0.1, grid_size=10):
         self.width, self.height = width, height
@@ -19,10 +20,12 @@ class Simulation:
 
         self.paused = True
 
+
     def generate_particles(self):
         particles = []
 
         for i in range(self.num_particles):
+            # generates the number of particles specified, with the parameters set here
             particle = Particle(position=(random.random(), random.random()),
                                 velocity=(1 - random.random() * 2, 1 - random.random() * 2),
                                 type=i % self.num_types,
@@ -34,6 +37,7 @@ class Simulation:
 
         return particles
 
+
     def add_to_grid(self, particle):
         # calculates cell coordinates by scaling position by the grid size and wraps around due to mod if applicable
         grid_x = int(particle.position[0] * self.grid_size) % self.grid_size
@@ -41,6 +45,7 @@ class Simulation:
 
         # adds particle to appropriate cell 
         self.cells[(grid_x, grid_y)].append(particle)
+
 
     def update(self, dt):
         # clears grid for updating particle positions in one step
@@ -51,45 +56,48 @@ class Simulation:
             p.update_position(dt, self.time_factor)
             self.add_to_grid(p)
 
-        # checks each particle for interactions against others in its own and neighboring cells 
+        # calculates particle interactions within current and neighboring cells
         for (grid_x, grid_y), particles in self.cells.items():
             for p1 in particles:
                 # checks current and surrounding cells
-                for dx in (-1, 0, 1):
-                    for dy in (-1, 0, 1):
+                for dx in (-1, 0, 1):       # checks self, left & right
+                    for dy in (-1, 0, 1):   # checks self, top & bottom
                         neighbor_cell = (grid_x + dx, grid_y + dy)
-                        # checks for neighboring cell
+                        # checks for existing, neighboring cells
                         if neighbor_cell in self.cells:
                             for p2 in self.cells[neighbor_cell]:
-                                # no self interaction
-                                if p1 is not p2:
+                                if p1 is not p2:    # no self interaction
                                     force_x, force_y = self.interaction_matrix.calculate_force(p1, p2)
                                     p1.apply_force(force_x, force_y)
-        
+
         self.enforce_boundaries()
         
+
     def enforce_boundaries(self):
+        # enforces boundaries if particles are out of bounds on either axis
         for p in self.particles:
             if p.position[0] <= 0 or p.position[0] >= 1:
+                # position is rounded to nearest axis
                 p.position[0] = round(p.position[0])
+                # velocity is inverted (particles bounce off of boundary)
                 p.velocity[0] = -p.velocity[0]
                 
             if p.position[1] <= 0 or p.position[1] >= 1:
                 p.position[1] = round(p.position[1])
                 p.velocity[1] = -p.velocity[1]
-            # if p.position[0] <= 0 or p.position[0] >= 1:
-            #     p.position[0] = abs(1 - round(p.position[0]))
-
-            # if p.position[1] <= 0 or p.position[1] >= 1:
-            #     p.position[1] = abs(round(p.position[1]) - 1)
             
+
     def start_simulation(self):
+        # generates particles if there are currently none
         self.paused = False
-        if len(self.particles) == 0:  # no particles generated
+        if len(self.particles) == 0:    
             self.particles = self.generate_particles()
 
+
     def stop_simulation(self):
-        self.paused = True
+        self.paused = True              
+
 
     def reset_simulation(self):
-        self.particles = []
+        # removes all particles
+        self.particles = []              
