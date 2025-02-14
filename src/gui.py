80b64ac5652920611bffe47ba964a9454df6b987
marvinkuckel <153325399+marvinkuckel@users.noreply.github.com>
2025-02-14 20:18:48 +0100

    didnt notice i made a mistake while mergin main into here

--- src/gui.py
+++ src/gui.py
@@ -151,12 +151,10 @@ class GUI:
             "Click Exit to leave the simulation.",
             "Thank you!",
             "",
-            10*"_" + "Matrix Controls" + 10*"_",
+            "_____ Matrix Controls _____",
             "Increase attraction - left mouse button / scroll upwards",
             "Increase repulsion - right mouse button / scroll downwards"
         ]
-
-        total_text_height = sum(font.get_height() + 10 for line in instruction_parts) + header_font.get_height()
 
         y_offset = self.instruction_rect.top + 70
 
@@ -186,92 +184,6 @@ class GUI:
             
         # set the height so the text fits inside
         self.instruction_rect.height = y_offset - self.instruction_rect.top + 10
-
-        if self.interactions_interface.fields:
-            last_field_bottom = list(self.interactions_interface.fields.values())[-1].bottom
-            self.instruction_rect = pygame.Rect(self.screen_width - self.control_panel_width + 10, 
-                last_field_bottom + 20, self.control_panel_width - 20, 250)
-        else:
-            self.instruction_rect = pygame.Rect(self.screen_width - self.control_panel_width + 10, 
-                                                180, self.control_panel_width - 20, 250)
-
-    def draw_instruction(self):
-        pygame.draw.rect(self.screen, self.colors['christmas-grey'], self.instruction_rect)
-        pygame.draw.rect(self.screen, (250, 5, 80), self.instruction_rect, 3)
-
-        font = pygame.font.Font(None, 23)
-        header_font = pygame.font.Font(None, 28)
-        header_font.set_bold(True)
-        header_font.set_italic(True)
-        
-        y_offset = self.instruction_rect.top + 25
-
-        header_parts = ["Welcome to the", "Particle", "Life", "Simulator", "!"]
-        segment_colors = [
-            self.colors['christmas-white'],
-            self.colors['christmas-red'],
-            self.colors['christmas-gold'],
-            self.colors['christmas-green'],
-            self.colors['christmas-white']
-        ]
-
-        x_offset = self.instruction_rect.centerx - sum(header_font.size(word)[0] for word in header_parts) / 2
-
-        for idx, part in enumerate(header_parts):
-            shadow_surface = header_font.render(part, True, (0, 0, 0))
-            shadow_rect = shadow_surface.get_rect(topleft=(x_offset + 1, y_offset + 1))
-            self.screen.blit(shadow_surface, shadow_rect)
-
-            part_surface = header_font.render(part, True, segment_colors[idx])
-            part_rect = part_surface.get_rect(topleft=(x_offset, y_offset))
-            self.screen.blit(part_surface, part_rect)
-
-            x_offset += part_surface.get_width()
-
-        y_offset += header_font.get_height()
-
-        color_words = {
-            "Start": self.colors['christmas-green'],
-            "Stop": self.colors['christmas-gold'],
-            "Reset": self.colors['christmas-red'],
-            "Exit": self.colors['christmas-blue']
-        }
-
-        instruction_parts = [
-            "Click Start to activate the particles.",
-            "While they are moving, you can press Stop to pause the simulation.",
-            "To clear the screen and restart, use Reset button.",
-            "Click Exit to leave the simulation.",
-            "Thank you!"
-        ]
-
-        total_text_height = sum(font.get_height() + 10 for line in instruction_parts) + header_font.get_height()
-
-        y_offset = self.instruction_rect.top + 80
-
-        x_offset = self.instruction_rect.left - 20
-
-        for line in instruction_parts:
-            words = line.split(" ")
-            centered_x_offset = x_offset + (self.instruction_rect.width - sum(font.size(word)[0] for word in words)) / 2
-
-            for word in words:
-                if word.strip("'") in color_words:
-                    color = color_words[word.strip("'")]
-                else:
-                    color = self.colors['christmas-white']
-
-                shadow_surface = font.render(word, True, (0, 0, 0))
-                shadow_rect = shadow_surface.get_rect(topleft=(centered_x_offset + 1, y_offset + 1))
-                self.screen.blit(shadow_surface, shadow_rect)
-
-                word_surface = font.render(word, True, color)
-                word_rect = word_surface.get_rect(topleft=(centered_x_offset, y_offset))
-                self.screen.blit(word_surface, word_rect)
-
-                centered_x_offset += word_surface.get_width() + font.size(" ")[0]
-
-            y_offset += font.get_height() + 15
 
     def initiate_buttons(self, simulation_controlls, h_padding = 60):
         # setup parameters for button initiation
