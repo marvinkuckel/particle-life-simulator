93a761e049f101a86918ccbc5ce09ac853ec6a30
Lilith Schmidl <lilith.schmidl@study.hs-duesseldorf.de>
2025-02-16 17:29:25 +0100

    modified particle.py to only render particle movement if higher than set threshold, to visually remove small movements

--- src/particle.py
+++ src/particle.py
@@ -1,4 +1,3 @@
-import random
 import pygame
 
 
@@ -9,11 +8,14 @@ class Particle:
         self.position = position
         self.velocity = velocity
         self.friction = friction
-        self.force_scaling = force_scaling
-     
+        self.force_scaling = force_scaling          
+        self.last_render_position = tuple(position) # tracks particles last rendered position
+
 
     def apply_force(self, force_x, force_y):
+        # applies force and the scaling factor to current velocity
         self.velocity = [self.velocity[0] + force_x*self.force_scaling, self.velocity[1] + force_y*self.force_scaling]
+
 
     def update_position(self, dt, time_factor):
         # update velocity based on applied friction
@@ -25,8 +27,20 @@ class Particle:
                         self.position[1] + self.velocity[1] * dt * time_factor]
 
 
-    def draw(self, screen, screen_width, screen_height, color):
-        # Draws the particle on the screen
-        pygame.draw.circle(screen, color, radius = self.size,
-                           center=(self.position[0] * screen_width,
-                                   self.position[1] * screen_height))
+    def draw(self, screen, screen_width, screen_height, color, threshold=0.001):  
+        # particles movement since last render
+        dx = abs(self.position[0] - self.last_render_position[0])
+        dy = abs(self.position[1] - self.last_render_position[1])
+
+        # only updates rendered position if above threshold
+        if dx > threshold or dy > threshold:
+            self.last_render_position = self.position
+
+        # conversion to screen coordinates
+        draw_x = round(self.last_render_position[0] * screen_width)
+        draw_y = round(self.last_render_position[1] * screen_height)
+
+        # draws the particle
+        pygame.draw.circle(screen, color, (draw_x, draw_y), self.size)
+        
+    
